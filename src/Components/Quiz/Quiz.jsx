9fccccc05f96e79c630d5094af34dd921dfa5f27
drvince00 <drvince00@gmail.com>
2024-09-18 21:40:34 +0900

    작업파일 올리기

---
+++ src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,115 @@
+import React, { useRef, useState } from 'react';
+import './Quiz.css';
+import { data } from '../../assets/data';
+
+export default function Quiz() {
+  let [index, setIndex] = useState(0);
+  const [question, setQuestion] = useState(data[index]);
+  const [lock, setLock] = useState(false);
+  const [score, setScore] = useState(0);
+  const [result, setResult] = useState(false);
+
+  const option1 = useRef(null);
+  const option2 = useRef(null);
+  const option3 = useRef(null);
+  const option4 = useRef(null);
+
+  const option_array = [option1, option2, option3, option4];
+
+  const checkAns = (e, ans) => {
+    if (!lock) {
+      if (question.ans === ans) {
+        e.target.classList.add('correct');
+        setScore(prev=>prev+1);
+      } else {
+        e.target.classList.add('wrong');
+        option_array[question.ans - 1].current.classList.add('correct');
+      }
+      setLock(true);
+    }
+  };
+
+  const next = () => {
+    if (lock) {
+      if (index === data.length - 1) {
+        setResult(true);
+        return 0;
+      }
+      setIndex(++index);
+      // setIndex(prev=>prev+1);
+      setQuestion(data[index]);
+      setLock(false);
+      option_array.map((option) => {
+        option.current.classList.remove('wrong');
+        option.current.classList.remove('correct');
+        return null;
+      });
+    }
+  };
+
+  const reset = () => {
+    setIndex(0);
+    setQuestion(data[0]);
+    setScore(0);
+    setLock(false);
+    setResult(false);
+  }
+  return (
+    <div className="container">
+      <h1>Quiz App</h1>
+      <hr />
+      {result ? (
+        <>
+          <h2>
+            You scored {score} out of {data.length}.
+          </h2>
+          <button onClick={reset}>Reset</button>
+        </>
+      ) : (
+        <>
+          <h2>
+            {index + 1}. {question.question}
+          </h2>
+          <ul>
+            <li
+              ref={option1}
+              onClick={(e) => {
+                checkAns(e, 1);
+              }}
+            >
+              {question.option1}
+            </li>
+            <li
+              ref={option2}
+              onClick={(e) => {
+                checkAns(e, 2);
+              }}
+            >
+              {question.option2}
+            </li>
+            <li
+              ref={option3}
+              onClick={(e) => {
+                checkAns(e, 3);
+              }}
+            >
+              {question.option3}
+            </li>
+            <li
+              ref={option4}
+              onClick={(e) => {
+                checkAns(e, 4);
+              }}
+            >
+              {question.option4}
+            </li>
+          </ul>
+          <button onClick={next}>Next</button>
+          <div className="index">
+            {index + 1} of {data.length} questions
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
